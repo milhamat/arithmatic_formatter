f0689e896a88f20fb3d5abaca873ca5365799fa5
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2021-10-24 15:27:09 +0700

    try making at the arithmetic_arranger first

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,11 @@
 def arithmetic_arranger(problems):
-    arranged_problems = problems
+    arranged_problems = []
 
-    return arranged_problems
+    for data in problems:
+        arranged_problems.append(data)
+
+    print(arranged_problems)
+
+    # return arranged_problems
+
+arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
