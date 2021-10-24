c77fa2577b1811ff31dd77f7713081b5705677b5
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2021-10-24 22:39:28 +0700

    removing unused variables

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -4,11 +4,8 @@ def formatter(list):
     split_str = sprt.rsplit()  # split the string into list
 
     # third make new variable for container
-    a = None
-    b = None
     result = None
-    nota = ''
-
+    
     # forth separate the which is addition or subtraction
     if split_str[1] == '+':
         total = int(split_str[0]) + int(split_str[2])
