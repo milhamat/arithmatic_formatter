ab7dda4f204ca0c4c6620656fa95ffb64c6d7839
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2021-10-24 21:27:17 +0700

    remove all comment and renaming a variable to spilt_str

--- engine_1.py
+++ engine_1.py
@@ -4,20 +4,13 @@ lis2 = ['70 - 22']
 
 def formatter(list):
     sprt = ' '.join(list)  # split the list into string
-    a = sprt.rsplit()  # split the string into list
+    split_str = sprt.rsplit()  # split the string into list
 
-    # print(type(sprt))  # debug the type of sprt
-
-    # print(sprt)  # debug the result of sprt
-
-    # print(a)  # debug the result of a
-
-    # print(type(a))  # debug the type of a
-    if a[1] == '+':
-        total = int(a[0]) + int(a[2])
+    if split_str[1] == '+':
+        total = int(split_str[0]) + int(split_str[2])
         print(total)
-    elif a[1] == '-':
-        total = int(a[0]) - int(a[2])
+    elif split_str[1] == '-':
+        total = int(split_str[0]) - int(split_str[2])
         print(total)
 
 
