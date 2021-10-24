1771973fec41c626f644adefcdf6ae9335c252a7
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2021-10-24 21:32:31 +0700

    make slit every single variable and make return function as tuple of all particle variable

--- engine_1.py
+++ engine_1.py
@@ -5,16 +5,26 @@ lis2 = ['70 - 22']
 def formatter(list):
     sprt = ' '.join(list)  # split the list into string
     split_str = sprt.rsplit()  # split the string into list
+    a = None
+    b = None
+    result = None
+    nota = ''
 
     if split_str[1] == '+':
         total = int(split_str[0]) + int(split_str[2])
-        print(total)
+        result = total
     elif split_str[1] == '-':
         total = int(split_str[0]) - int(split_str[2])
-        print(total)
+        result = total
+
+    a = split_str[0]
+    b = split_str[2]
+    nota = split_str[1]
+
+    return result, a, b, nota  # it will return as atuple
 
 
 
-formatter(lis)
-formatter(lis2)
+print(formatter(lis))
+# formatter(lis2)
 
