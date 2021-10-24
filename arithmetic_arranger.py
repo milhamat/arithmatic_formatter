0d6e2d3984f79bd651f5b55f666b1021a11b85c5
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2021-10-24 22:10:59 +0700

    add first formatting list

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,6 +1,11 @@
 def arithmetic_arranger(problems):
+    # first format the list data input
+    new_list = [problems[i:i + 1] for i in range(0, len(problems), 1)]
+    print(new_list)
 
 
-    
+
+
+
 
 arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
