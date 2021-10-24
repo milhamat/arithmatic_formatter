a41ea8d1899ec0f763c40d41074652e990f31dd9
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2021-10-24 16:06:32 +0700

    modified engine_1
    add new lis2
    make the code into function
    also making some logic
    last call the function with specific list

--- engine_1.py
+++ engine_1.py
@@ -1,15 +1,27 @@
 lis = ['32 + 50']
+lis2 = ['70 - 22']
 
-sprt = ' '.join(lis)  # split the list into string
-a = sprt.rsplit()  # split the string into list
 
-# print(type(sprt))  # debug the type of sprt
+def formatter(list):
+    sprt = ' '.join(list)  # split the list into string
+    a = sprt.rsplit()  # split the string into list
 
-# print(sprt)  # debug the result of sprt
+    # print(type(sprt))  # debug the type of sprt
 
-# print(a)  # debug the result of a
+    # print(sprt)  # debug the result of sprt
 
-# print(type(a))  # debug the type of a
+    # print(a)  # debug the result of a
 
-total = int(a[0]) + int(a[2])
-print(total)
+    # print(type(a))  # debug the type of a
+    if a[1] == '+':
+        total = int(a[0]) + int(a[2])
+        print(total)
+    elif a[1] == '-':
+        total = int(a[0]) - int(a[2])
+        print(total)
+
+
+
+formatter(lis)
+formatter(lis2)
+
