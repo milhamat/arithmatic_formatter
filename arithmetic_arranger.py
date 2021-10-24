5cfb576d78f47b94a2fa0b234b7a38631b9ee8d7
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2021-10-24 22:14:20 +0700

    move the experimentation code into one

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,7 +1,29 @@
 def arithmetic_arranger(problems):
     # first format the list data input
     new_list = [problems[i:i + 1] for i in range(0, len(problems), 1)]
-    print(new_list)
+
+    # second split the data
+    sprt = ' '.join(new_list)  # split the list into string
+    split_str = sprt.rsplit()  # split the string into list
+
+    # third make new variable for container
+    a = None
+    b = None
+    result = None
+    nota = ''
+
+    # forth separate the which is addition or subtraction
+    if split_str[1] == '+':
+        total = int(split_str[0]) + int(split_str[2])
+        result = total
+    elif split_str[1] == '-':
+        total = int(split_str[0]) - int(split_str[2])
+        result = total
+
+    # fifth store the result inside the new variable 
+    a = split_str[0]
+    b = split_str[2]
+    nota = split_str[1]
 
 
 
