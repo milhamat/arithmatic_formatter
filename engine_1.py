ac1b03cb08d12677f1806bc44d752cc50ec5bf91
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2021-10-24 16:00:25 +0700

    commenting the code

--- engine_1.py
+++ engine_1.py
@@ -3,13 +3,13 @@ lis = ['32 + 50']
 sprt = ' '.join(lis)  # split the list into string
 a = sprt.rsplit()  # split the string into list
 
-print(type(sprt))
-	
-print(sprt)
+# print(type(sprt))  # debug the type of sprt
 
-print(a)
+# print(sprt)  # debug the result of sprt
 
-print(type(a))
+# print(a)  # debug the result of a
+
+# print(type(a))  # debug the type of a
 
 total = int(a[0]) + int(a[2])
 print(total)
