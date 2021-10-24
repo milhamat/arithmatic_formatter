9fa3fe49e1ab9383d0c09122bdc78accc5e1943c
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2021-10-24 15:35:19 +0700

    split the data into three staged list

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,11 +1,11 @@
 def arithmetic_arranger(problems):
     arranged_problems = []
 
-    for data in problems:
+    for datas in problems:
+        data = datas.split()
         arranged_problems.append(data)
 
     print(arranged_problems)
-
     # return arranged_problems
 
 arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
