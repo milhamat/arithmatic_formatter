d7ad2b3029c78ee34be72df1ae8110338d7f54de
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2021-10-24 22:08:58 +0700

    clean the data

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,11 +1,6 @@
 def arithmetic_arranger(problems):
-    arranged_problems = []
 
-    for data in problems:
-        arranged_problems.append(data)
 
-    print(arranged_problems)
-
-    # return arranged_problems
+    
 
 arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
