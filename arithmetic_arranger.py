fe3e2f0acd19c4104bdd8574095628b431f8eb44
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2021-10-30 18:45:51 +0700

    try to make same expected print out

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -43,9 +43,10 @@ def arithmetic_arranger(problems):
                 {rslt}
                 '''
         print(frmt)
+        # return frmt
 
 
 
 
 
-arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+#arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from arithmetic_arranger import arithmetic_arranger
 
 
-# print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
 
 
+# expected_output = '   32         1      45      123      988\n- 698    - 3801    + 43    +  49    +  40\n-----    ------    ----    -----    -----\n -666     -3800      88      172     1028'
